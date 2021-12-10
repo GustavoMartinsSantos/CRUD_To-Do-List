55c89cb08a33900e54f329817911563d5b9b7356
Gustavo Martins <gustavo_mts2005@hotmail.com>
2021-12-09 21:48:37 -0300

    Added the first tasks to the database

---
+++ docker-compose/mysql/insertsDatabase.sql
@@ -0,0 +1,6 @@
+USE db_tasks;
+
+INSERT INTO tbl_tasks VALUES(NULL, 'Química Atividade do Seneca', NULL, '2021-12-09 19:30:59', 0);
+INSERT INTO tbl_tasks VALUES(NULL, 'Projeto Arduino Natal', NULL, '2021-12-08 23:59:59', 2);
+INSERT INTO tbl_tasks VALUES(NULL, 'Prova Português', NULL, '2021-12-09 11:30:00', 2);
+INSERT INTO tbl_tasks VALUES(NULL, 'Projeto CRUD PWII', NULL, '2021-12-09 10:40:00', 1);
