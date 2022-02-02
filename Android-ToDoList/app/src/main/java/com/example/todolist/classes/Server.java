07a884437475b0a4ada57b0fd2d1cfd6fd502aa5
Gustavo Martins <gustavo_mts2005@hotmail.com>
2022-02-02 10:59:12 -0300

    Modified the Execute method to allow null post and get arrays

--- Android-ToDoList/app/src/main/java/com/example/todolist/classes/Server.java
+++ Android-ToDoList/app/src/main/java/com/example/todolist/classes/Server.java
@@ -2,40 +2,39 @@ package com.example.todolist.classes;
 
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Objects;
-
-import static com.example.todolist.classes.Task.getTasksByString;
-
 public abstract class Server {
     private static String getURLStringByMultArray (String[][] array) {
         String getString = "?";
 
         for(String[] getArray : array) {
-            getString += getArray[0] + "=" + getArray[1];
+            getString += getArray[0] + "=" + getArray[1] + "&";
         }
 
         return getString;
     }
 
     public static String Execute (String link, String[][] getArray, String[][] postArray) {
+        int postArrayLength = 0;
         String result = new String();
         PutData putData;
-        int postArrayLength = postArray.length;
 
-        String[] fields = new String[postArrayLength];
-        String[] values = new String[postArrayLength];
+        if(getArray != null)
+            link += getURLStringByMultArray(getArray);
+        if(postArray != null)
+            postArrayLength = postArray.length;
+
+        String[] fields = new String[postArrayLength + 1];
+        String[] values = new String[postArrayLength + 1];
 
         for(int c = 0; c < postArrayLength; c++) {
             fields[c] = postArray[c][0];
             values[c] = postArray[c][1];
         }
 
-        fields[postArrayLength - 1] = "Mobile";
-        values[postArrayLength - 1] = "true";
+        fields[postArrayLength] = "Mobile";
+        values[postArrayLength] = "true";
 
-        putData = new PutData(link + getURLStringByMultArray(getArray), "POST", fields, values);
+        putData = new PutData(link, "POST", fields, values);
         if(putData.startPut()) {
             if(putData.onComplete()) {
                 result = putData.getResult();
